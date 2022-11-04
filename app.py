b42a74e2d050611326fb9a11138c95fe558f9413
algonacci <algonacci1@gmail.com>
2022-11-04 15:29:37 +0700

    finishing

--- app.py
+++ app.py
@@ -1,15 +1,46 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, send_file
+from werkzeug.utils import secure_filename
 import module as md
 
 app =  Flask(__name__)
+app.config["ALLOWED_EXTENSIONS"] = set(["pdf"])
+app.config["UPLOAD_FOLDERS"] = "uploads/"
+
+def allowed_file(filename):
+    return (
+        "." in filename
+        and filename.rsplit(".", 1)[1] in app.config["ALLOWED_EXTENSIONS"]
+    )
 
 @app.route("/")
 def main():
     return render_template("index.html")
 
-@app.route("/result")
+@app.route("/result", methods=["GET", "POST"])
 def result():
-    return render_template("result.html")
+    if request.method == "POST":
+        document = request.files["file"]
+        if document and allowed_file(document.filename):
+            filename = secure_filename(document.filename)
+            document.filename.replace(' ', '_')
+            document.save(os.path.join(app.config["UPLOAD_FOLDERS"], filename))
+            document_file = os.path.join(app.config["UPLOAD_FOLDERS"], filename)
+            preprocessed = md.preprocessing(document_file)
+            document_stores = md.document_store(preprocessed)
+            qag_pipeline = md.question_generator_pipeline(document_stores)
+            file_excel = md.question_generator(document_stores, qag_pipeline)
+            return render_template("result.html", file_excel=file_excel)
+        else:
+            return render_template("index.html", message="Please upload a PDF file!")    
+    else:
+        return render_template("index.html", message="Please upload a PDF file!")
+
+@app.route("/download_excel/<excel>")
+def download_excel(excel):
+    excel = 'static/'+excel
+    print(excel)
+    return send_file(excel, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(
--- module.py
+++ module.py
@@ -5,3 +5,57 @@ from haystack.pipelines import QuestionAnswerGenerationPipeline
 import time
 import tqdm
 import xlsxwriter
+
+def preprocessing(file_path):
+    pdf_converter = PDFToTextConverter(
+        remove_numeric_tables=True, valid_languages=["en"])
+    converted = pdf_converter.convert(file_path=file_path, meta={
+                                      "company": "Company_1", "processed": False})
+    preprocessor = PreProcessor(
+        split_by="word", split_length=200, split_overlap=10)
+    preprocessed = preprocessor.process(converted)
+    return preprocessed
+
+def document_store(document_preprocessed):
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    document_store = FAISSDocumentStore(
+        sql_url='sqlite:///'+timestr+'_document_store.db', faiss_index_factory_str="Flat", return_embedding=True)
+    document_store.delete_documents()
+    document_store.write_documents(document_preprocessed)
+    return document_store
+
+def question_generator_pipeline(document_store):
+    reader = FARMReader(
+        model_name_or_path='deepset/tinyroberta-squad2', use_gpu=True)
+    question_generator = QuestionGenerator()
+    qag_pipeline = QuestionAnswerGenerationPipeline(question_generator, reader)
+    return qag_pipeline
+
+
+def question_generator(document_store, qag_pipeline):
+    row = 1
+    column = 0
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    filename_excel = timestr + '.xls'
+
+    workbook = xlsxwriter.Workbook('static/'+filename_excel)
+    worksheet = workbook.add_worksheet('Sheet 1')
+    worksheet.write(0, 0, 'Question')
+    worksheet.write(0, 1, 'Answer')
+    worksheet.write(0, 2, 'Context')
+
+    for idx, document in enumerate(tqdm.tqdm(document_store)):
+        res = qag_pipeline.run(documents=[document])
+        for i in range(0, len(res['queries'])):
+            query = res['queries'][i]
+            worksheet.write(row, column, query)
+
+            answer = res['answers'][i][0].answer
+            worksheet.write(row, column + 1, answer)
+
+            contexts = res['answers'][i][0].context
+            worksheet.write(row, column + 2, contexts)
+            row += 1
+            
+    workbook.close()
+    return filename_excel
